fc48b1e4757e7d607bd73053251280780a5269cf
artemissvt <artemis.sevastiadi@gmail.com>
2025-02-14 20:23:53 +0200

    first correct commit

--- del.py
+++
@@ -1,8 +0,0 @@
-print("Hello World!")
-student = "computing student"
-name = input("Type your name: ")
-age = float(input("Type your age: "))
-print("Welcome", name)
-print("You are", age, "y.o.")
-print("You are a", student)
-
