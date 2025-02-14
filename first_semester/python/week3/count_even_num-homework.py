ac43c7a0c9afbb0676face9fb3a72bb880c9aa5e
artemissvt <artemis.sevastiadi@gmail.com>
2025-02-14 20:31:04 +0200

    se parakalw as einai swsto

--- first_semester/python/week3/count_even_num-homework.py
+++
@@ -1,9 +0,0 @@
-import random
-random_numbers = [random.randint(1,100) for _ in range(20)]
-odd_nums = 0
-for num in random_numbers:
-    if num % 2 != 0:
-        odd_nums += 1
-print ('Random numbers:', random_numbers)
-print ('Even numbers:', odd_nums)
-
