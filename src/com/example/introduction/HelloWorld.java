541150d77e268132b3cbdebcd3b2edb4e5ca555b
artemissvt <artemis.sevastiadi@gmail.com>
2025-02-23 13:03:24 +0200

    first commit

---
+++ src/com/example/introduction/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.example.introduction;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello ble!");
+    }
+}
---
+++ src/wk1/Wktest.java
@@ -0,0 +1,7 @@
+package wk1;
+
+public class Wktest {
+    public static void main(String[] args) {
+        System.out.println("Wktest");
+    }
+}
