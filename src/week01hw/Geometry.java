151041283a8aea52f6c9866c263691bab094f093
artemissvt <artemis.sevastiadi@gmail.com>
2025-04-17 00:35:37 +0300

    geometry hw week01

--- src/week01hw/Geometry.java
+++ src/week01hw/Geometry.java
@@ -1,37 +1,32 @@
 package week01hw;
 
-import java.util.Scanner;
-
 public class Geometry {
     public static void main(String[] args) {
-        Scanner r1 = new Scanner(System.in);
-        System.out.println("Type the height of the first rectangle:");
-        int heightR1 = r1.nextInt();
-        //System.out.println(heightR1);
-        System.out.println( );
+        int width = 10;
+        int height = 5;
 
+        //top row
+        for (int i = 0; i < width; i++) {
+            System.out.print("* ");
+        }
+        System.out.println();
 
-        System.out.println("Type the width of the first rectangle:");
-        int widthR1 = r1.nextInt();
-        System.out.println(widthR1);
-
-        Scanner r2 = new Scanner(System.in);
-        System.out.println("Type the height of the second rectangle:");
-        int heightR2 = r2.nextInt();
-        System.out.println(heightR2);
-
-        System.out.println("Type the width of the second rectangle:");
-        int widthR2 = r2.nextInt();
-        System.out.println(widthR2);
-
-        /*for (int i = 0; i < heightR1; i++) {
+        //middle row
+        for (int i = 0; i < height - 2; i++) {
+            System.out.print("* ");
+            for (int j = 0; j < width - 2; j++) {
+                System.out.print("  ");
+            }
             System.out.println("*");
-            if (.startsWith);
+        }
 
-            //for (int j = 0; j < widthR1; j++) {
-            //    System.out.print("*");
-           // }
-        }*/
+
+        //bottom rows
+        for (int i = 0; i < width; i++) {
+            System.out.print("* ");
+        }
+        System.out.println();
+
 
     }
 }
