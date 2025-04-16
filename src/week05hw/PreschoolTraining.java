daf7eea705b107ce6210f49031aa733da974c44c
artemissvt <artemis.sevastiadi@gmail.com>
2025-04-16 14:28:22 +0300

    preschool images hw first part

---
+++ src/week05hw/PreschoolTraining.java
@@ -0,0 +1,65 @@
+package week05hw;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PreschoolTraining {
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Preschool Training");
+
+        JLabel northlabel = new JLabel("Welcome to the picture game");
+        window.add(northlabel, BorderLayout.NORTH);
+        JButton clickmebtn = new JButton("Click here to continue");
+        JPanel centerPanel = new JPanel(new GridLayout(20,20));
+
+        centerPanel.add(clickmebtn);
+        window.add(centerPanel, BorderLayout.CENTER);
+        clickmebtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                centerPanel.removeAll();
+
+                JLabel newlabel = new JLabel("The rules are simple:");
+                JLabel ruleone = new JLabel("You write the first letter of the animal you're seeing on the picture.");
+                JLabel ruletwo = new JLabel("There is one picture for each letter of the alphabet");
+                JButton clicktostart = new JButton("Click here to start");
+
+                centerPanel.add(newlabel);
+                centerPanel.add(ruleone);
+                centerPanel.add(ruletwo);
+                centerPanel.add(clicktostart);
+
+                window.remove(northlabel);
+                centerPanel.revalidate();
+                centerPanel.repaint();
+
+                JLabel newlabel2 = new JLabel();
+
+                clicktostart.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        centerPanel.removeAll();
+
+                        centerPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+                        ImageIcon image = new ImageIcon("images/ant.jpg");
+                        newlabel2.setIcon(image);
+                        centerPanel.add(newlabel2);
+
+                        JTextField nameField = new JTextField(15);
+                        centerPanel.add(nameField);
+
+                        centerPanel.revalidate();
+                        centerPanel.repaint();
+                    }
+                });
+            }
+        });
+
+
+        window.setSize(500, 500);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setBackground(new Color(20, 200, 181));
+        window.setVisible(true);
+    }
+}
