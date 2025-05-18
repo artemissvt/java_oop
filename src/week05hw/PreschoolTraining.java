f7eea238d5a1d734accc649c82fdca4fa85f1afd
artemissvt <artemis.sevastiadi@gmail.com>
2025-05-18 16:31:36 +0300

    correct hw 5

--- src/week05hw/PreschoolTraining.java
+++ src/week05hw/PreschoolTraining.java
@@ -18,7 +18,9 @@ public class PreschoolTraining {
         window.add(centerPanel, BorderLayout.CENTER);
         clickmebtn.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
+                window.getContentPane().removeAll();
                 centerPanel.removeAll();
+                centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
 
                 JLabel newlabel = new JLabel("The rules are simple:");
                 JLabel ruleone = new JLabel("You write the first letter of the animal you're seeing on the picture.");
@@ -37,12 +39,14 @@ public class PreschoolTraining {
 
                 JLabel newlabel2 = new JLabel();
                 JLabel resultlabel = new JLabel();
+                JLabel resultlabel2 = new JLabel();
+                JLabel resultlabel3 = new JLabel();
 
                 clicktostart.addActionListener(new ActionListener() {
                     public void actionPerformed(ActionEvent e) {
+                        window.getContentPane().removeAll();
                         centerPanel.removeAll();
-
-                        centerPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+                        centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
 
                         ImageIcon image = new ImageIcon("images/ant.jpg");
                         newlabel2.setIcon(image);
@@ -50,15 +54,23 @@ public class PreschoolTraining {
                         centerPanel.add(newlabel2);
 
                         JTextField nameField = new JTextField(15);
+                        nameField.setMaximumSize(new Dimension(200, 25));
                         nameField.setAlignmentX(Component.CENTER_ALIGNMENT);
+                        centerPanel.add(Box.createRigidArea(new Dimension(0, 10)));
                         centerPanel.add(nameField);
 
                         JButton checkanswer = new JButton("Check answer");
                         checkanswer.setAlignmentX(Component.CENTER_ALIGNMENT);
+                        centerPanel.add(Box.createRigidArea(new Dimension(0, 10)));
                         centerPanel.add(checkanswer);
 
                         resultlabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+                        centerPanel.add(Box.createRigidArea(new Dimension(0, 10)));
                         centerPanel.add(resultlabel);
+
+                        window.add(centerPanel);
+                        window.revalidate();
+                        window.repaint();
 
                         checkanswer.addActionListener(new ActionListener() {
                             public void actionPerformed(ActionEvent e) {
@@ -72,16 +84,98 @@ public class PreschoolTraining {
                                 centerPanel.add(continuebutton, BorderLayout.SOUTH);
                                 continuebutton.addActionListener(new ActionListener() {
                                     public void actionPerformed(ActionEvent e) {
-                                        centerPanel.remove(resultlabel);
-                                        centerPanel.remove(checkanswer);
-                                        centerPanel.remove(continuebutton);
+                                        window.getContentPane().removeAll();
+                                        centerPanel.removeAll();
+                                        centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
 
-                                        centerPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
                                         ImageIcon image = new ImageIcon("images/horse.jpg");
                                         newlabel2.setIcon(image);
                                         newlabel2.setAlignmentX(Component.CENTER_ALIGNMENT);
                                         centerPanel.add(newlabel2);
+
                                         JTextField nameField = new JTextField(15);
+                                        nameField.setMaximumSize(new Dimension(200, 25));
+                                        nameField.setAlignmentX(Component.CENTER_ALIGNMENT);
+                                        centerPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+                                        centerPanel.add(nameField);
+
+                                        JButton checkanswer = new JButton("Check answer");
+                                        checkanswer.setAlignmentX(Component.CENTER_ALIGNMENT);
+                                        centerPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+                                        centerPanel.add(checkanswer);
+
+                                        resultlabel2.setAlignmentX(Component.CENTER_ALIGNMENT);
+                                        centerPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+                                        centerPanel.add(resultlabel2);
+
+                                        window.add(centerPanel);
+                                        window.revalidate();
+                                        window.repaint();
+
+                                        checkanswer.addActionListener(new ActionListener() {
+                                            public void actionPerformed(ActionEvent e) {
+                                                if (nameField.getText().toLowerCase().equals("h")) {
+                                                    resultlabel2.setText("Correct!");
+                                                } else {
+                                                    resultlabel2.setText("Wrong!");
+                                                }
+                                                JButton continuebutton = new JButton("Continue");
+                                                continuebutton.setAlignmentX(Component.CENTER_ALIGNMENT);
+                                                centerPanel.add(continuebutton, BorderLayout.SOUTH);
+
+                                                continuebutton.addActionListener(new ActionListener() {
+                                                    public void actionPerformed(ActionEvent e) {
+                                                        window.getContentPane().removeAll();
+                                                        centerPanel.removeAll();
+                                                        centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
+
+                                                        ImageIcon image = new ImageIcon("images/dog.jpg");
+                                                        newlabel2.setIcon(image);
+                                                        newlabel2.setAlignmentX(Component.CENTER_ALIGNMENT);
+                                                        centerPanel.add(newlabel2);
+
+                                                        JTextField nameField = new JTextField(15);
+                                                        nameField.setMaximumSize(new Dimension(200, 25));
+                                                        nameField.setAlignmentX(Component.CENTER_ALIGNMENT);
+                                                        centerPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+                                                        centerPanel.add(nameField);
+
+                                                        JButton checkanswer = new JButton("Check answer");
+                                                        checkanswer.setAlignmentX(Component.CENTER_ALIGNMENT);
+                                                        centerPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+                                                        centerPanel.add(checkanswer);
+
+                                                        resultlabel3.setAlignmentX(Component.CENTER_ALIGNMENT);
+                                                        centerPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+                                                        centerPanel.add(resultlabel3);
+
+                                                        window.add(centerPanel);
+                                                        window.revalidate();
+                                                        window.repaint();
+
+                                                        checkanswer.addActionListener(new ActionListener() {
+                                                            public void actionPerformed(ActionEvent e) {
+                                                                if (nameField.getText().toLowerCase().equals("d")) {
+                                                                    resultlabel3.setText("Correct!");
+                                                                } else {
+                                                                    resultlabel3.setText("Wrong!");
+                                                                }
+                                                                JButton finishbutton = new JButton("Finish");
+                                                                finishbutton.setAlignmentX(Component.CENTER_ALIGNMENT);
+                                                                centerPanel.add(finishbutton, BorderLayout.SOUTH);
+                                                                finishbutton.addActionListener(new ActionListener() {
+                                                                    public void actionPerformed(ActionEvent e) {
+                                                                        System.exit(0);
+                                                                        centerPanel.add(finishbutton);
+                                                                    }
+                                                                });
+                                                            }
+                                                        });
+
+                                                    }
+                                                });
+                                            }
+                                        });
                                     }
                                 });
                             }
